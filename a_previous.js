186d43d166900704a4ed5a9a932b6de869ab85c6
belltoy <belltoy@gmail.com>
2014-11-24 15:42:06 +0800

    Simplify codes

--- a_previous.js
+++ a_previous.js
@@ -1,58 +1,8 @@
-var b;
-// make 49 warnings
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-b = 1
-var a = {id: 4, name: 'belltoy'};
+var a = {};
 
 for(var i in a) {
-    // make it the 50th warning, and jshint will skip this file left the uncleaned environment.
+    // Make forin check just reach the maxerr limit,
+    // and jshint will skip this file left the uncleaned environment.
     // if (a.hasOwnProperty(i)) {
         console.log('key:', i, 'value:', a[i]);
     // }
